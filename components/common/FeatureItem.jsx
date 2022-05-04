9dfad0e3d244fda7e8015045e9a473ed37ec77d4
minhnh <minhnh.da@gmail.com>
2022-05-04 16:03:40 +0900

    fix: Fix heroicons disappear on Safari

--- components/common/FeatureItem.jsx
+++ components/common/FeatureItem.jsx
@@ -1,7 +1,7 @@
 const FeatureItem = ({ icon, text }) => {
   return (
     <div className="flex items-center">
-      <div className="flex shrink-0 items-center justify-center w-12 h-12 p-2 rounded-full bg-slate-blue">
+      <div className="flex shrink-0 w-12 h-12 p-2 rounded-full bg-slate-blue">
         { icon }
       </div>
       <p className="ml-4 md:max-w-default text-desktop-paragraph text-slate-body">
