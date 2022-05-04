c768a08fa8bb286b3c6e55cc3e441215ca09ae37
minhnh <minhnh.da@gmail.com>
2022-05-04 16:55:49 +0900

    fix: Fix feature item spacing on Safari iOS 14

--- components/common/FeatureItem.jsx
+++ components/common/FeatureItem.jsx
@@ -1,7 +1,9 @@
+import styles from "./FeatureItem.module.css";
+
 const FeatureItem = ({ icon, text }) => {
   return (
-    <div className="flex items-center">
-      <div className="flex shrink-0 w-12 h-12 p-2 rounded-full bg-slate-blue">
+    <div className={ `flex items-center ${ styles.mb_equal_flex_gap_6 }` }>
+      <div className="shrink-0 w-12 h-12 p-2 rounded-full bg-slate-blue">
         { icon }
       </div>
       <p className="ml-4 md:max-w-default text-desktop-paragraph text-slate-body">
