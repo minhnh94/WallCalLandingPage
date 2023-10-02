699e6a07df48f74552f86ce7bfd855416790a8e9
minhnh <minhnh.da@gmail.com>
2023-10-03 08:26:24 +0900

    add anchor to FAQ section

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -2,7 +2,7 @@ import FAQItem from "./common/FAQItem";
 
 const FAQ = ({ items }) => {
   return (
-    <section className="py-20 font-dm-sans bg-slate-light">
+    <section id="faq" className="py-20 font-dm-sans bg-slate-light">
       <div className="container px-4 mx-auto">
         <div className="max-w-2xl mx-auto text-center mb-12">
           <h2 className="mt-2 mb-4 text-4xl lg:text-5xl font-medium font-heading">Frequently asked questions</h2>
