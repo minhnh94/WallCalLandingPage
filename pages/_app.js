02a8cd5b241ef5aaca787fcb6b107b1edaff58bf
minhnh <minhnh.da@gmail.com>
2025-09-14 15:23:39 +0900

    add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider
+      domain="wallcal.app"
+      scriptProps={{ defer: true }}
+      trackOutboundLinks={true}
+      taggedEvents={true}
+      customDomain="https://plausible.tsukie.com"
+      selfHosted={true}
+    >
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
